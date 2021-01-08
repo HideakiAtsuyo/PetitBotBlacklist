c4c420f9870ceb854cde2e126ed4d765ee9dcbb1
janast1975 <72787716+janast1975@users.noreply.github.com>
2021-01-08 20:32:11 +0100

    Update unblacklist.js

--- commands/unblacklist.js
+++ commands/unblacklist.js
@@ -1,40 +1,34 @@
-const Discord = require("discord.js");
-const botconfig = require("../botconfig");
-let purple = botconfig.purple;
-let grade = require('../grade.json')
-let blacklist = require('../blacklist.json')
+let grade = require('../data/grade.json')
+let blacklist = require('../data/blacklist.json')
 let fs = require("fs")
 
 module.exports.run = async (bot, message, args) => {
 
+  if (grade[message.author.id].permlevel < 40) return message.channel.send(`Cette commande est réservée au staff du bot.`)
+  let unblacklistuser = message.guild.member(message.mentions.users.first()) || bot.users.get(args[0]);
+  if (!unblacklistuser) return message.channel.send(`Utilisateur Incorrect.`)
 
-  if(grade[message.author.id].permlevel < 40) return message.channel.send(`Cette commande est réservée au staff du bot.`)
-    let unblacklistuser = message.guild.member(message.mentions.users.first()) || bot.users.get(args[0]);
-  if(!unblacklistuser) return message.channel.send(`Utilisateur Incorrect.`)
-
-  if(!blacklist[unblacklistuser.id]){
-   blacklist[unblacklistuser.id] = {
-     user: "Non"
-  };
+  if (!blacklist[unblacklistuser.id]) {
+    blacklist[unblacklistuser.id] = {
+      user: "Non"
+    };
   }
 
-  if(!grade[unblacklistuser.id]){
-   grade[unblacklistuser.id] = {
-     permlevel: "0"
-  };
+  if (!grade[unblacklistuser.id]) {
+    grade[unblacklistuser.id] = {
+      permlevel: "0"
+    };
   }
 
- if(blacklist[unblacklistuser.id].user === "Non") return message.channel.send(`Cette personne n'est pas blacklist.`)
+  if (blacklist[unblacklistuser.id].user === "Non") return message.channel.send(`Cette personne n'est pas blacklist.`)
 
   blacklist[unblacklistuser.id].user = "Non"
 
   message.channel.send(`${unblacklistuser} a été retiré de la blacklist.`)
 
-  fs.writeFile("./blacklist.json", JSON.stringify(blacklist), (err) => {
-    if(err) console.log(err)
+  fs.writeFile("./data/blacklist.json", JSON.stringify(blacklist), (err) => {
+    if (err) console.log(err)
   });
-
-
 }
 
 module.exports.help = {
